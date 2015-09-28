e9cf2663f6d7e92cf42dc693779ab28a9a86ee3c
Weghst <kevinz@weghst.com>
2015-09-28 17:53:22 +0800

    修改Spring JDBC实现
    修改Spring JDBC实现采用NamedParameterJdbcTemplate方式

--- core/src/main/java/com/weghst/pine/repository/impl/ConfigurationRepositoryImpl.java
+++ core/src/main/java/com/weghst/pine/repository/impl/ConfigurationRepositoryImpl.java
@@ -1,18 +1,19 @@
 package com.weghst.pine.repository.impl;
 
-import com.weghst.pine.ConfigUtils;
-import com.weghst.pine.Pines;
 import com.weghst.pine.domain.Config;
 import com.weghst.pine.repository.ConfigurationRepository;
 import com.weghst.pine.repository.DeletedException;
 import com.weghst.pine.repository.FindException;
 import com.weghst.pine.repository.SavedException;
+import com.weghst.pine.repository.UpdatedException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.sql.ResultSet;
@@ -24,26 +25,25 @@ public class ConfigurationRepositoryImpl implements ConfigurationRepository {
 
     private static final Logger LOG = LoggerFactory.getLogger(ConfigurationRepositoryImpl.class);
 
-    private static final String SAVE_SQL = "insert into t_config(key,value,remarks,comments,needReboot,lastUpdatedTime) values(?,?,?,?,?,?)";
-    private static final String DELETE_BY_KEY_SQL = "delete from t_config where key=?";
-    private static final String UPDATE_SQL = "update t_config set value=?,remarks=?,comments=?,needReboot=?," +
-            "lastUpdatedTime=UNIX_TIMESTAMP(),lastBut2UpdatedTime=lastUpdatedTime where key=?";
-    private static final String UPDATE_VALUE_SQL = "update t_config set value=?, where key=?";
+    private static final String SAVE_SQL = "insert into t_config(key,value,remarks,comments,needReboot)" +
+            " values(:key,:value,:remarks,:comments,:needReboot)";
+    private static final String DELETE_BY_KEY_SQL = "delete from t_config where key=:key";
+    private static final String UPDATE_SQL = "update t_config set value=:value,remarks=:remarks,comments=:comments,needReboot=:needReboot," +
+            "lastUpdatedTime=UNIX_TIMESTAMP(),lastBut2UpdatedTime=lastUpdatedTime where key=:key";
+    private static final String UPDATE_VALUE_SQL = "update t_config set value=:value, where key=:key";
     private static final String GET_SQL = "select `key`,`value`,`remarks`,`comments`,`needReboot`," +
             "`lastUpdatedTime`,`lastBut2UpdatedTime` from t_config where key=?";
     private static final String FIND_ALL_SQL = "select `key`,`value`,`remarks`,`comments`,`needReboot`," +
             "`lastUpdatedTime`,`lastBut2UpdatedTime` from t_config";
 
     @Autowired
-    private JdbcTemplate jdbcTemplate;
+    private NamedParameterJdbcTemplate jdbcTemplate;
 
     @Override
     public void save(Config config) {
         LOG.debug("Save config: {}", config);
 
-        int r = jdbcTemplate.update(SAVE_SQL, config.getKey(), config.getValue(),
-                config.getRemarks(), config.getComments(), config.isNeedReboot(),
-                Pines.unixTimestamp());
+        int r = jdbcTemplate.update(SAVE_SQL, new BeanPropertySqlParameterSource(config));
         if (r != 1) {
             throw new SavedException("保存配置失败. 预期影响[1]条记录, 实际影响[" + r + "]条记录");
         }
@@ -53,7 +53,9 @@ public class ConfigurationRepositoryImpl implements ConfigurationRepository {
     public void delete(String key) {
         LOG.debug("Delete config by key: {}", key);
 
-        int r = jdbcTemplate.update(DELETE_BY_KEY_SQL, key);
+        MapSqlParameterSource parameterSource = new MapSqlParameterSource();
+        parameterSource.addValue("key", key);
+        int r = jdbcTemplate.update(DELETE_BY_KEY_SQL, parameterSource);
         if (r != 1) {
             throw new DeletedException("删除配置失败. 预期影响[1]条记录, 实际影响[" + r + "]条记录");
         }
@@ -63,10 +65,9 @@ public class ConfigurationRepositoryImpl implements ConfigurationRepository {
     public void update(Config config) {
         LOG.debug("Update config: {}", config);
 
-        int r = jdbcTemplate.update(UPDATE_SQL, config.getValue(), config.getRemarks(),
-                config.getComments(), config.isNeedReboot(), config.getKey());
+        int r = jdbcTemplate.update(UPDATE_SQL, new BeanPropertySqlParameterSource(config));
         if (r != 1) {
-            throw new DeletedException("修改配置失败. 预期影响[1]条记录, 实际影响[" + r + "]条记录");
+            throw new UpdatedException("修改配置失败. 预期影响[1]条记录, 实际影响[" + r + "]条记录");
         }
     }
 
@@ -74,9 +75,12 @@ public class ConfigurationRepositoryImpl implements ConfigurationRepository {
     public void updateValue(String key, String value) {
         LOG.debug("Update config value: {key={}, value={}}", key, value);
 
-        int r = jdbcTemplate.update(UPDATE_VALUE_SQL, value, key);
+        MapSqlParameterSource parameterSource = new MapSqlParameterSource();
+        parameterSource.addValue("key", key);
+        parameterSource.addValue("value", value);
+        int r = jdbcTemplate.update(UPDATE_VALUE_SQL, parameterSource);
         if (r != 1) {
-            throw new DeletedException("修改配置失败. 预期影响[1]条记录, 实际影响[" + r + "]条记录");
+            throw new UpdatedException("修改配置失败. 预期影响[1]条记录, 实际影响[" + r + "]条记录");
         }
     }
 
--- core/src/main/java/com/weghst/pine/repository/impl/UserRepositoryImpl.java
+++ core/src/main/java/com/weghst/pine/repository/impl/UserRepositoryImpl.java
@@ -11,10 +11,11 @@ import com.weghst.pine.repository.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.PreparedStatementCreator;
-import org.springframework.jdbc.core.PreparedStatementSetter;
 import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
@@ -26,41 +27,32 @@ public class UserRepositoryImpl implements UserRepository {
 
     private static final Logger LOG = LoggerFactory.getLogger(UserRepositoryImpl.class);
 
-    private static final String SAVE_SQL = "insert into t_user(email,password,emailValid,createdTime) values(?,?,?,?)";
-    private static final String DELETE_BY_ID_SQL = "delete from t_user where id=?";
-    private static final String UPDATE_BY_ID_SQL = "update t_user set email=?,password=?,emailValid=? where id=?";
-    private static final String UPDATE_EMAIL_VALID_BY_EMAIL = "update t_user set emailValid=? where email=?";
-    private static final String GET_BY_ID_SQL = "select * from t_user where id=?";
-    private static final String GET_BY_EMAIL_SQL = "select * from t_user where email=?";
+    private static final String SAVE_SQL = "insert into t_user(email,password,emailValid) values(:email,:password,:emailValid)";
+    private static final String DELETE_BY_ID_SQL = "delete from t_user where id=:id";
+    private static final String UPDATE_BY_ID_SQL = "update t_user set email=:email,password=:password,emailValid=:emailValid where id=:id";
+    private static final String UPDATE_EMAIL_VALID_BY_EMAIL = "update t_user set emailValid=:emailValid where email=:email";
+    private static final String GET_BY_ID_SQL = "select * from t_user where id=:id";
+    private static final String GET_BY_EMAIL_SQL = "select * from t_user where email=:email";
 
     // ============================ UserTempField ================================//
 
-    private static final String SAVE_OR_UPDATE_USER_TEMP_FIELD_SQL =
-            "insert into t_user_temp_field(uid,field,value,createdTime,survivalMillis) values(?,?,?,?,?)" +
-            " on duplicate key update value=?,createdTime=?,survivalMillis=?";
-    private static final String DELETE_USER_TEMP_FIELD_SQL = "delete from t_user_temp_field where uid=? and field=?";
-    private static final String CLEAN_USER_TEMP_FIELD_SQL = "delete from t_user_temp_field where (createdTime+survivalMillis)<?";
-    private static final String GET_USER_TEMP_FIELD_SQL = "select value,createdTime,survivalMillis where uid=? and field=?";
+    private static final String SAVE_OR_UPDATE_USER_TEMP_FIELD_SQL = "insert into t_user_temp_field(uid,field,value,createdTime,survivalMillis)" +
+            " values(:uid,:field,:value,:createdTime,:survivalMillis)" +
+            " on duplicate key update value=:value,createdTime=:createdTime,survivalMillis=:survivalMillis";
+    private static final String DELETE_USER_TEMP_FIELD_SQL = "delete from t_user_temp_field where uid=:uid and field=:field";
+    private static final String CLEAN_USER_TEMP_FIELD_SQL = "delete from t_user_temp_field where (createdTime+survivalMillis) < :expiredTime";
+    private static final String GET_USER_TEMP_FIELD_SQL = "select value,createdTime,survivalMillis where uid=:uid and field=:field";
 
     @Autowired
-    private JdbcTemplate jdbcTemplate;
+    private NamedParameterJdbcTemplate jdbcTemplate;
 
     @Override
     public void save(User user) {
         LOG.debug("Save user: {}", user);
 
         KeyHolder keyHolder = new GeneratedKeyHolder();
-        int r = jdbcTemplate.update(conn -> {
-            PreparedStatement ps = conn.prepareStatement(SAVE_SQL);
 
-            int i = 1;
-            ps.setString(i++, user.getEmail());
-            ps.setString(i++, user.getPassword());
-            ps.setBoolean(i++, user.isEmailValid());
-            ps.setLong(i++, Pines.unixTimestamp());
-            return ps;
-        }, keyHolder);
-
+        int r = jdbcTemplate.update(SAVE_SQL, new BeanPropertySqlParameterSource(user), keyHolder);
         if (r != 1) {
             throw new SavedException("保存用户失败. 预期影响[1]条记录, 实际影响[" + r + "]条记录");
         }
@@ -72,7 +64,9 @@ public class UserRepositoryImpl implements UserRepository {
     public void delete(int id) {
         LOG.debug("Delete user by id [{}]", id);
 
-        int r = jdbcTemplate.update(DELETE_BY_ID_SQL, id);
+        MapSqlParameterSource parameterSource = new MapSqlParameterSource();
+        parameterSource.addValue("id", id);
+        int r = jdbcTemplate.update(DELETE_BY_ID_SQL, parameterSource);
         if (r != 1) {
             throw new DeletedException("删除用户失败. 预期影响[1]条记录, 实际影响[" + r + "]条记录");
         }
@@ -82,14 +76,7 @@ public class UserRepositoryImpl implements UserRepository {
     public void update(User user) {
         LOG.debug("Update user: {}", user);
 
-        int r = jdbcTemplate.update(UPDATE_BY_ID_SQL, ps -> {
-            int i = 1;
-            ps.setString(i++, user.getEmail());
-            ps.setString(i++, user.getPassword());
-            ps.setBoolean(i++, user.isEmailValid());
-            ps.setInt(i++, user.getId());
-        });
-
+        int r = jdbcTemplate.update(UPDATE_BY_ID_SQL, new BeanPropertySqlParameterSource(user));
         if (r != 1) {
             throw new UpdatedException("修改用户失败. 预期影响[1]条记录, 实际影响[" + r + "]条记录");
         }
@@ -99,12 +86,10 @@ public class UserRepositoryImpl implements UserRepository {
     public void updateEmailValid(String email, boolean emailValid) {
         LOG.debug("Update user emailValid: [email={}, emailValid={}]", email, emailValid);
 
-        int r = jdbcTemplate.update(UPDATE_EMAIL_VALID_BY_EMAIL, ps -> {
-            int i = 1;
-            ps.setBoolean(i++, emailValid);
-            ps.setString(i++, email);
-        });
-
+        MapSqlParameterSource parameterSource = new MapSqlParameterSource();
+        parameterSource.addValue("email", email);
+        parameterSource.addValue("emailValid", emailValid);
+        int r = jdbcTemplate.update(UPDATE_EMAIL_VALID_BY_EMAIL, parameterSource);
         if (r != 1) {
             throw new UpdatedException("修改用户邮件验证失败. 预期影响[1]条记录, 实际影响[" + r + "]条记录");
         }
@@ -112,11 +97,8 @@ public class UserRepositoryImpl implements UserRepository {
 
     @Override
     public User get(int id) {
-        PreparedStatementCreator psc = conn -> {
-            PreparedStatement ps = conn.prepareStatement(GET_BY_ID_SQL);
-            ps.setInt(1, id);
-            return ps;
-        };
+        MapSqlParameterSource parameterSource = new MapSqlParameterSource();
+        parameterSource.addValue("id", id);
 
         ResultSetExtractor<User> rse = rs -> {
             User user = null;
@@ -131,16 +113,13 @@ public class UserRepositoryImpl implements UserRepository {
             return user;
         };
 
-        return jdbcTemplate.query(psc, rse);
+        return jdbcTemplate.query(GET_BY_ID_SQL, parameterSource, rse);
     }
 
     @Override
     public User get(String email) {
-        PreparedStatementCreator psc = conn -> {
-            PreparedStatement ps = conn.prepareStatement(GET_BY_EMAIL_SQL);
-            ps.setString(1, email);
-            return ps;
-        };
+        MapSqlParameterSource parameterSource = new MapSqlParameterSource();
+        parameterSource.addValue("email", email);
 
         ResultSetExtractor<User> rse = rs -> {
             User user = null;
@@ -155,26 +134,15 @@ public class UserRepositoryImpl implements UserRepository {
             return user;
         };
 
-        return jdbcTemplate.query(psc, rse);
+        return jdbcTemplate.query(GET_BY_EMAIL_SQL, parameterSource, rse);
     }
 
     @Override
     public void saveOrUpdate(UserTempField userTempField) {
         LOG.debug("SaveOrUpdate user temp field: {}", userTempField);
 
-        int r = jdbcTemplate.update(SAVE_OR_UPDATE_USER_TEMP_FIELD_SQL, ps -> {
-            int i = 1;
-            ps.setInt(i++, userTempField.getUid());
-            ps.setString(i++, userTempField.getField());
-            ps.setString(i++, userTempField.getValue());
-            ps.setLong(i++, Pines.currentTimeMillis());
-            ps.setLong(i++, userTempField.getSurvivalMillis());
-
-            ps.setString(i++, userTempField.getValue());
-            ps.setLong(i++, Pines.currentTimeMillis());
-            ps.setLong(i++, userTempField.getSurvivalMillis());
-        });
-
+        int r = jdbcTemplate.update(SAVE_OR_UPDATE_USER_TEMP_FIELD_SQL,
+                new BeanPropertySqlParameterSource(userTempField));
         if (r != 1) {
             throw new SavedException("保存用户临时字段失败. 预期影响[1]条记录, 实际影响[" + r + "]条记录");
         }
@@ -184,12 +152,10 @@ public class UserRepositoryImpl implements UserRepository {
     public void deleteUserTempField(int uid, String field) {
         LOG.debug("Delete user temp field: [uid={}, field={}]", uid, field);
 
-        int r = jdbcTemplate.update(DELETE_USER_TEMP_FIELD_SQL, ps -> {
-            int i = 1;
-            ps.setInt(i++, uid);
-            ps.setString(i++, field);
-        });
-
+        MapSqlParameterSource parameterSource = new MapSqlParameterSource();
+        parameterSource.addValue("uid", uid);
+        parameterSource.addValue("field", field);
+        int r = jdbcTemplate.update(DELETE_USER_TEMP_FIELD_SQL, parameterSource);
         if (r != 1) {
             throw new DeletedException("删除用户临时字段失败. 预期影响[1]条记录, 实际影响[" + r + "]条记录");
         }
@@ -197,26 +163,23 @@ public class UserRepositoryImpl implements UserRepository {
 
     @Override
     public int cleanUserTempField() {
-        int r = jdbcTemplate.update(CLEAN_USER_TEMP_FIELD_SQL, Pines.currentTimeMillis());
-        LOG.debug("清理过期用户临时字段. 影响[{}]条记录", r);
+        MapSqlParameterSource parameterSource = new MapSqlParameterSource();
+        parameterSource.addValue("expiredTime", Pines.currentTimeMillis());
+        int r = jdbcTemplate.update(CLEAN_USER_TEMP_FIELD_SQL, parameterSource);
+        LOG.info("清理过期用户临时字段. 影响[{}]条记录", r);
 
         return r;
     }
 
     @Override
     public UserTempField getUserTempField(int uid, String field) {
-        PreparedStatementCreator psc = conn -> {
-            PreparedStatement ps = conn.prepareStatement(GET_USER_TEMP_FIELD_SQL);
-
-            int i = 1;
-            ps.setInt(i++, uid);
-            ps.setString(i++, field);
-            return ps;
-        };
+        MapSqlParameterSource parameterSource = new MapSqlParameterSource();
+        parameterSource.addValue("uid", uid);
+        parameterSource.addValue("field", field);
 
         ResultSetExtractor<UserTempField> rse = rs -> {
             UserTempField userTempField = null;
-            if(rs.next()) {
+            if (rs.next()) {
                 userTempField = new UserTempField();
                 userTempField.setUid(uid);
                 userTempField.setField(field);
@@ -226,7 +189,7 @@ public class UserRepositoryImpl implements UserRepository {
             }
             return userTempField;
         };
-        return jdbcTemplate.query(psc, rse);
+        return jdbcTemplate.query(GET_USER_TEMP_FIELD_SQL, parameterSource, rse);
     }
 
 }
